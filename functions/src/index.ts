6975072a8ed83f836a9d4640f659dd3738396cad
wilson <l7960261@gmail.com>
2019-06-12 13:21:05 +0800

    Add /version/check

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,3 +28,32 @@ app.get('/currency/exchange', (req, res) => {
 
   res.status(200).send(result);
 });
+
+app.post('/version/check', (req, res) => {
+  const _version = '1.0.0';
+  const _android_apk = 'https://test1.4q.sk/flutter_hello_world.apk';
+  const _ios_apk = '';
+  const { version, os } = req.body;
+  const isLatest = _version == version;
+  const platform = os ? os.toLowerCase() : 'android';
+  var result = {
+    link: '',
+    message: ''
+  };
+
+  switch (platform) {
+    case 'android':
+      result.link = isLatest ? '' : _android_apk;
+      result.message = isLatest ? 'It\'s latest version.' : '';
+      break;
+    case 'ios':
+      result.link = isLatest ? '' : _ios_apk;
+      result.message = isLatest ? 'It\'s latest version.' : '';
+      break;
+    default:
+      result.message = `${platform} is not supported.`;
+      break;
+  }
+  
+  res.status(200).send(result);
+});
